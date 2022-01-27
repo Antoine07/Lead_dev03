5ce0851d30b6e3de78975624c12738dee9ccb363
Antoine07 <antoine.lucsko@gmail.com>
2022-01-27 11:53:42 +0100

    split

--- 01_PHP/01_introduction/Exercices/split_array.php
+++ 01_PHP/01_introduction/Exercices/split_array.php
@@ -11,3 +11,17 @@ $numbers = [1,2,3];
 array_pop($numbers);  // 1 [2,3]
 array_pop($numbers);  // 2 [3]
 array_pop($numbers);  // 3 []
+
+function split_array(array $numbers, int $pos): array
+{
+    if ($pos > count($numbers)) return $numbers;
+
+    list($start, $i) = [[], 0];
+
+    while ($i++ <= $pos) 
+        $start[] = array_shift($numbers); // $start[] <=> array_push
+
+    return [$start, $numbers];
+}
+
+var_dump(split_array(numbers: [4,6,9, 17], pos: 2  ));

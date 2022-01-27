da79c242b3079a0996d7a2962b191ba67e8e6655
Antoine07 <antoine.lucsko@gmail.com>
2022-01-27 16:49:40 +0100

    closure

---
+++ 01_PHP/02_Closure_portee/Examples/closure.php
@@ -0,0 +1,14 @@
+<?php
+
+$message = "Hello";
+
+$showMessage = function() use ($message) : void{
+   $newMessage = "World";
+
+   function() use ($newMessage, $message){
+       echo $message . $newMessage;
+   };
+
+};
+
+$showMessage();
